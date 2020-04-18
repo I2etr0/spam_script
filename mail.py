f692d3fe576b3974680958f3634823a28250a525
I2etr0 <47482322+I2etr0@users.noreply.github.com>
2020-04-18 15:18:20 +0500

    Update script's logic

---
+++ mail.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+
+def sendMail():
+    msg = MIMEMultipart()
+
+    # General settings
+    msg['from'] = 'form@example.com'
+    msg['to'] = 'to@example.com'
+    msg['subject'] = 'user login'
+    message = 'User log in system'
+
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Create server
+    server = smtplib.SMTP('yourmailserver:port')
+
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+    print("Successfully send mail to %s:" % (msg['to']))
+
+
+def spam():
+
+    min = 1
+    max = int(input('How much messages you want send: '))
+
+    while max >= min:
+        sendMail()
+        min += 1
+
+
+spam()
